558abb4a3e74599afa2c3a3517ee817351f96fc6
9oelM <hj923@hotmail.com>
2023-11-13 22:50:13 +0800

    chore: set up eslint on vscode

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": ["react-app"],
+  "rules": {
+    // "example-rule": "warn"
+  },
+  "overrides": [
+    {
+      "files": ["**/*.ts?(x)"],
+      "rules": {
+        // "exmaple-typescript-only-rule": "warn"
+      }
+    }
+  ]
+}
