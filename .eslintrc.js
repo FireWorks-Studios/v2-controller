d0c9b97473883b1461bc45bfb068307d348b928a
9oelM <hj923@hotmail.com>
2024-01-10 18:35:25 +0800

    chore: add unused* rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,36 @@
 module.exports = {
   "extends": ["react-app"],
+  "plugins": ["unused-imports"],
   "rules": {
     // "example-rule": "warn"
+    "no-unused-vars": ["error", {
+      "vars": "all",
+      "varsIgnorePattern": "^_",
+      "args": "after-used",
+      "argsIgnorePattern": "^_"
+    }],
+    "unused-imports/no-unused-imports": ["error", {
+      "vars": "all",
+      "varsIgnorePattern": "^_",
+      "args": "after-used",
+      "argsIgnorePattern": "^_"
+    }],
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "varsIgnorePattern": "^_",
+        "args": "after-used",
+        "argsIgnorePattern": "^_"
+      }
+    ]
   },
   "overrides": [
     {
       "files": ["**/*.ts?(x)"],
       "rules": {
-        // "exmaple-typescript-only-rule": "warn"
+        // make @typescript-eslint/no-unused-vars an error
+        // "@typescript-eslint/no-unused-vars": ["error"]
       }
     }
   ]
